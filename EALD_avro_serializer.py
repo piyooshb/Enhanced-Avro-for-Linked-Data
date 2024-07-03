15a1cc85893e39c7b06f288c8e80fd53f39caea6
piyooshb <learn110022@gmail.com>
2024-07-03 15:38:45 -0700

    Create EALD_Serializer.py

---
+++ EALD_avro_serializer.py
@@ -0,0 +1,16 @@
+import os
+import json
+import avro
+from avro.datafile import DataFileWriter
+
+
+'''
+    Publisher :- Serialize Avro schema and Avro Payload to write Person Event Avro File 
+'''
+
+schema = avro.schema.parse(open(name_and_address.avsc, "rb").read())
+payload_data = json.load(open(name_and_address_payload.json))
+
+writer = DataFileWriter(open(person_event.avro, "wb"), DatumWriter(), schema)
+writer.append(payload_data)
+writer.close()
