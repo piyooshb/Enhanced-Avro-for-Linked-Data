b8154fb08c1ef06e6a8891ede1415ca0f2692a59
piyooshb <learn110022@gmail.com>
2024-07-08 12:01:32 -0700

    Update MALD_Deserializer.py

--- MALD_Deserializer.py
+++ MALD_Deserializer.py
@@ -13,9 +13,22 @@ import pyld.jsonld as jsonld
         2. Convert JSON-LD document to N-Triples 
 '''
 
-context = json.load(open(name_and_address_context.json))
+schema_fnm = 'name_and_address.avsc'
+serialized_avro_fnm = 'person_event.avro'
+context_fnm = 'name_and_address_context.json'
+jsonld_fnm = 'name_and_address.jsonld'
+nt_fnm = 'name_and_address.nt'
 
-reader = DataFileReader(open(person_event.avro, "rb"), DatumReader(name_and_address.avsc))
+curr_path = os.path.dirname(os.path.abspath(__file__))
+avro_schema = '{}\{}'.format(curr_path, schema_fnm)
+serialized_avro = '{}\{}.avro'.format(curr_path, serialized_avro_fnm)
+context_input = '{}\{}'.format(curr_path, context_fnm)
+jsonld_output = '{}\{}'.format(curr_path, jsonld_fnm)
+nt_output = '{}\{}'.format(curr_path, nt_fnm)
+
+context = json.load(open(context_input))
+
+reader = DataFileReader(open(serialized_avro, "rb"), DatumReader(avro_schema))
 
 for avro_payload in reader:
   jsonld_data = {"@context": context, "@graph": [avro_payload]}
@@ -23,7 +36,7 @@ for avro_payload in reader:
 reader.close()
 
 # Write JSON-LD document to a file
-with open(name_and_address.jsonld, 'w') as output_file:
+with open(jsonld_output, 'w') as output_file:
     output_file.write(json.dumps(jsonld_data, indent=4))
   
 # Convert JSON-LD to RDF format (Turtle)
@@ -37,5 +50,5 @@ graph.parse(data=rdf_data, format='nquads')
 nt_data = graph.serialize(format='nt')
 
 # Write N-Triples to a file
-with open(name_and_address.nt, "w") as outfile:
+with open(nt_output, "w") as outfile:
     outfile.write(nt_data)
